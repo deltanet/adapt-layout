337e8f7beb3ae07c401f8a069ba4ef8cd38df560
Robert Peek <RobertPeek@users.noreply.github.com>
2021-11-18 12:07:54 +0000

    CSS now handles the styling and layout

--- js/layout-iconView.js
+++ js/layout-iconView.js
@@ -23,22 +23,5 @@ export default class LayoutIconView extends Backbone.View {
     $(this.el).html(template(data)).prependTo('.' + this.id + ' > .component__inner' + ' > .component__header' + ' > .component__header-inner' + ' > .component__body');
 
     $('.' + this.id).addClass('is-layout-icon');
-
-    this.$el.imageready(this.setLayout.bind(this));
-  }
-
-  setLayout() {
-    const width = $(this.el).outerWidth();
-    const height = $(this.el).outerHeight();
-
-    if (Adapt.config.get('_defaultDirection') === 'ltr') {
-      const direction = 'left';
-    } else {
-      const direction = 'right';
-    }
-
-    $('.' + this.id).find('.component__body-inner').css('margin-'+direction, width);
-    $('.' + this.id).find('.component__instruction-inner').css('margin-'+direction, width);
-    $('.' + this.id).find('.component__body-inner').css('min-height', height);
   }
 }
